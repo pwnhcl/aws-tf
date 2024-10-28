60612761df6f15c9e089ed48c5e0751043b4b3d4
Prashant <pwnhcl@gmail.com>
2024-10-28 21:20:52 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.73.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0dee22c13ea7a9a67"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server"
+  }
+}
